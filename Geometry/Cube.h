0a69ce0dfbca5f989e41cb74d9a9f30c9b5fba15
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-12-30 21:07:47 +0530

    adding more code
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

---
+++ Geometry/Cube.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Cube {
+    public:
+        double getVolume();
+        double getSurfaceArea();
+        void setLength(double length);
+
+    private:
+        double length_;
+};
---
+++ Geometry/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "Cube.h"
+
+int main() {
+    Cube cube;
+
+    cube.setLength(3.5);
+    double vol = cube.getVolume();
+
+    std::cout << "Volume" << vol << std::endl;
+
+    return 0;
+}
